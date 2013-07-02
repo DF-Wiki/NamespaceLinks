cdc8f7dd82cb6685ef47bb6f8570c1a7361883f9
lethosor <stahlalan19@gmail.com>
2013-07-02 13:53:24 -0400

    Credits

---
+++ NamespaceLinks.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Extension:NamespaceLinks
+ *
+ * Allows links to default to different namespaces based on their namespace.
+ */
+
+$wgExtensionCredits['parserhook'][] = array(
+	'path' => __FILE__,
+	'name' => 'NamespaceLinks',
+	'author' => 'Lethosor',
+	'version' => '0.0',
+	'description' => 'Allows links to default to different namespaces based on their namespace.',
+	'url' => 'https://github.com/lethosor/mw-namespace-links',
+);
+
