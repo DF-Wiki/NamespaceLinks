8cc1c15d64781deed04756032a8c04f9304da675
lethosor <lethosor@gmail.com>
2022-12-21 18:22:09 -0500

    Correct version number to 0.3.2 to match git tags

--- NamespaceLinks.php
+++ NamespaceLinks.php
@@ -17,7 +17,7 @@ $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'NamespaceLinks',
 	'author' => 'Lethosor',
-	'version' => '0.1.2',
+	'version' => '0.3.2',
 	'description' => 'Allows links to default to different namespaces based on their namespace.',
 	'url' => 'https://github.com/DF-Wiki/NamespaceLinks',
 );
